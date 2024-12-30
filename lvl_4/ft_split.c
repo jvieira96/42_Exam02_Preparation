aefa8a05fa9b87d930e43616968814e4a67540c6
Joao Pedro Freire Vieira <jpedro-f@c2r5s11.42porto.com>
2024-12-30 15:52:58 +0000

    2

--- lvl_4/ft_split.c
+++ lvl_4/ft_split.c
@@ -13,47 +13,69 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ft_count_words(char *str)
+char *ft_strncpy(char *s1, char *s2, int n)
 {
-	int i = 0;
-	int	word_count = 0;
-	int	new_word = 0;
-	int	k;
+	int	i = 0;
 
-	if (!(str[i] == ' ' || (str[i] >= 0 && str[i] <= 13)))
+	while (i < n && s2[i])
 	{
+		s1[i] = s2[i];
 		i++;
-		word_count++;
 	}
-	while (str[i])
-	{
-		k = 0;
-		while (str[i] == ' ' || (str[i] >= 9 && str[i] <= 13))
-			{
-				i++;
-				new_word = 1;
-			}
-		if (!(str[i] == ' ' || (str[i] >= 9 && str[i] <= 13)) && str[i])
-		{
-			i++;
-			if (new_word == 1)
-			{			
-				word_count++;
-				new_word = 0;
-			}
-		}
-	}
-	return (word_count);
+	s1[i] = '\0';
+	return (s1);	
 }
 
 char **ft_split(char *str)
 {
-	
+	int		words = 0;
+	int		i = 0;
+	int		k = 0;
+	int		j = 0;
+	char	**strings;
+
+	while (str[i])
+	{
+		while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+			i++;
+		if (str[i])
+			words++;
+		while (str[i] && !(str[i] == 32 || (str[i] >= 9 && str[i] <= 13)))
+			i++;
+	}
+	i = 0;
+	strings = (char **) malloc(sizeof(char *) * (words + 1));
+	if (!strings)
+		return (NULL);
+	while (str[i])
+	{
+		while (str[i] == 32 || (str[i] >= 9 && str[i] <= 13))
+			i++;
+		k = i;
+		while (str[i] && !(str[i] == 32 || (str[i] >= 9 && str[i] <= 13)))
+			i++;
+		if (i > k)
+		{
+			strings[j] = (char *) malloc(sizeof(char) * ((i - k) + 1));
+			ft_strncpy(strings[j], &str[k], i - k);
+			j++;
+		}
+	}
+	strings[words] = NULL;
+	return (strings);
 }
 
-int main(int argc, char *argv[])
+/* int	main(int argc, char *argv[])
 {
+	int		i = 0;
+	char	**strings;
 	(void)argc;
-	printf("%d", ft_count_words(argv[1]));
+	strings = ft_split(argv[1]);
+	while (strings[i])
+	{
+		printf("%s", strings[i]);
+		printf("\n");
+		i++;
+	}
 	return (0);
-}
+} */
